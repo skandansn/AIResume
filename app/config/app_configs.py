bbb1c4d9177a50eab729839cc4782bd17aa3842d
Skandan Senthil Nathan <skandansn@protonmail.com>
2024-07-23 21:19:22 +0530

    change default resumename to mine

--- app/config/app_configs.py
+++ app/config/app_configs.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     app_name: str = "AIResume"
     gemini_api_key: str = "gemini_api_key"
-    output_resume_name: str = "AIResume"
+    output_resume_name: str = "SkandanSenthilNathan_Resume"
 
     model_config = SettingsConfigDict(env_file="app/config/.env")
 
