76e154eae93d4d1b2dd3108a7f658c9435fb5d7a
Skandan Senthil Nathan <skandansn@protonmail.com>
2024-07-25 08:25:25 +0530

    try setting samesite None (lax)

--- app/server.py
+++ app/server.py
@@ -52,7 +52,7 @@ def sign_up(input: input_models.SignUp, response: fastapi.Response):
 def sign_in(input: input_models.SignUp, response: fastapi.Response):
     user = sign_in_with_email_and_password(input.email, input.password)
     token = user.get("idToken")
-    response.set_cookie(key="authToken", value=token, httponly=True, samesite="none", secure=True)
+    response.set_cookie(key="authToken", value=token, httponly=True, samesite=None, secure=True)
     return user
 
 @app.post("/signOut")
